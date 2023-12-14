232ffedeae9977255279d8bf4038f33961cf5a61
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-14 11:33:10 +0530

    css tricks formationn changed

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import CircleText from "./Pages/Image-design/CircleText";
+import GhostText from "./Pages/Text-design/ghostTexts/GhostText";
 import BackImageText from "./Pages/Text-design/BackImageText/BackImageText";
 import BackVideoText from "./Pages/Text-design/BackVideoText/BackVideoText";
-import GhostText from "./Pages/Text-design/ghostTexts/GhostText";
-
 
 function App() {
   return (
     <div className="App">
-      <BackVideoText/>
-      
-      <BackImageText />
       <GhostText />
+      <BackImageText />
+      <BackVideoText />
       <CircleText />
     </div>
   );
