719c99c2d72cd397920a8ca15528b0e5927f9301
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-18 12:35:46 +0530

    New css for image move

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -3,16 +3,16 @@ import CircleText from "./Pages/Image-design/CircleText";
 import GhostText from "./Pages/Text-design/ghostTexts/GhostText";
 import BackImageText from "./Pages/Text-design/BackImageText/BackImageText";
 import BackVideoText from "./Pages/Text-design/BackVideoText/BackVideoText";
-import CyclingGradient from "./Pages/Text-design/CyclingGradient/CyclingGradient";
+import CyclingGradient from "./Pages/Image-design/CyclingGradient/CyclingGradient";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <GhostText />
+      <GhostText />
       <BackImageText />
       <BackVideoText />
-      <CircleText /> */}
+      <CircleText />
     <CyclingGradient />
     </div>
   );
