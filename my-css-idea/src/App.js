dd722aee607945970751ad3b8f5e9633c8100985
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-19 15:16:32 +0530

    Image shape

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import CircleText from "./Pages/Image-design/CircleText";
+
 import GhostText from "./Pages/Text-design/ghostTexts/GhostText";
 import BackImageText from "./Pages/Text-design/BackImageText/BackImageText";
 import BackVideoText from "./Pages/Text-design/BackVideoText/BackVideoText";
 import CyclingGradient from "./Pages/Image-design/CyclingGradient/CyclingGradient";
+import CircleText from "./Pages/Image-design/CircleText/CircleText";
 
 function App() {
   return (
---
+++ my-css-idea/src/Pages/Image-design/ImageShape/ImageShape.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ImageShape.module.css";
+
+const ImageShape = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default ImageShape;
