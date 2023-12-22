3f6d6d63c7ed5d2cc33461707cc0c284fab66af2
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-22 11:26:05 +0530

    Image filter effect is done

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -6,12 +6,14 @@ import BackVideoText from "./Pages/Text-design/BackVideoText/BackVideoText";
 import CyclingGradient from "./Pages/Image-design/CyclingGradient/CyclingGradient";
 import CircleText from "./Pages/Image-design/CircleText/CircleText";
 import ImageShape from "./Pages/Image-design/ImageShape/ImageShape";
+import FilterImage from "./Pages/Image-design/FilterImage/FilterImage";
 
 function App() {
   return (
     <div className="App">
       <GhostText />
       <ImageShape />
+      <FilterImage />
       <BackImageText />
       <BackVideoText />
       <CircleText />
---
+++ my-css-idea/src/Pages/Image-design/FilterImage/FilterImage.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styles from "./Filter.module.css";
+
+const FilterImage = () => {
+  return (
+    <div className={styles.cont}>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont1}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont2}
+        />
+      </div>
+
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont3}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont4}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont5}
+        />
+      </div>
+
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont6}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont7}
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="xyz"
+          className={styles.mainCont8}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FilterImage;
