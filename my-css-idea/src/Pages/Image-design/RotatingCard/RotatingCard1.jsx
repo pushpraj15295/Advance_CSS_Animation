a998b61c4cecf9cd6b4d2d94a570e5812e3a5aa7
pushpraj15295 <pushprajnda007@gmail.com>
2024-01-08 10:06:15 +0530

    video url change with some css properties

--- my-css-idea/src/Pages/Image-design/RotatingCard/RotatingCard1.jsx
+++ my-css-idea/src/Pages/Image-design/RotatingCard/RotatingCard1.jsx
@@ -19,11 +19,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("Summer season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
           <div
@@ -31,11 +31,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("fall season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
           <div
@@ -43,11 +43,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("Winter season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
           <div
@@ -55,11 +55,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("spring season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
           <div
@@ -67,11 +67,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("rainy season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
           <div
@@ -79,15 +79,14 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("This is your Land Earth")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
+              src="https://vod-progressive.akamaized.net/exp=1704702841~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3178%2F14%2F365890980%2F1509403930.mp4~hmac=0142c4553d5cc2ffe404e9f5ed93933bc76ae949da02999ed884dfce00b2653e/vimeo-prod-skyfire-std-us/01/3178/14/365890980/1509403930.mp4?filename=file.mp4"
             />
           </div>
         </div>
-
         {/* 2nd */}
         <div className={styles.card}>
           <div
@@ -95,11 +94,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("Summer season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
           <div
@@ -107,11 +106,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("fall season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
           <div
@@ -119,11 +118,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("Winter season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
           <div
@@ -131,11 +130,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("spring season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
           <div
@@ -143,11 +142,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("rainy season")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
           <div
@@ -155,11 +154,11 @@ const RotatingCardOne = () => {
             onClick={() => handleClick("This is your Land Earth")}
           >
             <video
-              width="178%"
+              width="100%"
               autoPlay
               muted
               loop
-              src="https://vod-progressive.akamaized.net/exp=1704656646~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F814%2F22%2F554074594%2F2620983736.mp4~hmac=78aab6f10b0d18a2529cbd1b244b1d0af8f094f24facde5b8ca66677a582a7db/vimeo-prod-skyfire-std-us/01/814/22/554074594/2620983736.mp4?filename=file.mp4"
+              src="https://cdn.pixabay.com/vimeo/357512275/wireframe-26097.mp4?width=1280&hash=d9015a9c5a81b9acfab99db4d2f08eb6c2a77932"
             />
           </div>
         </div>
