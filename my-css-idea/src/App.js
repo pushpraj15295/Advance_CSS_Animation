5c94fbf1ba34684b4c045524904586cc1febeb0e
Rishi Anand <rishi.a@honeysys.com>
2024-01-08 20:44:45 +0530

    nav dropdown animation

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -12,10 +12,12 @@ import Card3D from "./Pages/Image-design/3Dcard/Index";
 import RotatingCard from "./Pages/Image-design/RotatingCard/RotatingCard";
 import BackgroundImageUnderContent from "./Pages/video-animation/background-image/background-image";
 import RotatingCardOne from "./Pages/Image-design/RotatingCard/RotatingCard1";
+import DropdownAnimation from "./Pages/dropdown-animation/dropdown-animation";
 
 function App() {
   return (
     <div className="App">
+      <DropdownAnimation/>
       <BackgroundImageUnderContent />
       <GhostText />
       <ImageShape />
