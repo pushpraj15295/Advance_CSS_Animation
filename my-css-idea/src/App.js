a5c7e1f8e4127b5cdeb2c08e3ceff96e30b68bdc
pushpraj15295 <pushprajnda007@gmail.com>
2024-01-09 09:52:37 +0530

    Watch for the website with code formation

--- my-css-idea/src/App.js
+++ my-css-idea/src/App.js
@@ -10,14 +10,13 @@ import FilterImage from "./Pages/Image-design/FilterImage/FilterImage";
 import Loader from "./Pages/Loader/Loader";
 import Card3D from "./Pages/Image-design/3Dcard/Index";
 import RotatingCard from "./Pages/Image-design/RotatingCard/RotatingCard";
-import BackgroundImageUnderContent from "./Pages/video-animation/background-image/background-image";
 import RotatingCardOne from "./Pages/Image-design/RotatingCard/RotatingCard1";
 import DropdownAnimation from "./Pages/dropdown-animation/dropdown-animation";
 
 function App() {
   return (
     <div className="App">
-      <DropdownAnimation/>
+      <DropdownAnimation />
       <GhostText />
       <ImageShape />
       <Card3D />
--- my-css-idea/src/Pages/video-animation/background-image/background-image.jsx
+++ my-css-idea/src/Pages/video-animation/background-image/background-image.jsx
@@ -1,35 +1,50 @@
-import React from 'react'
-import styles from "./background-image.module.css"
-import Logo from "../../../images/logo-1.svg"
-import Mouse from "../../../images/mouse.svg"
-import Carpet_Roll from "../../../videos/trafic.mp4"
+import React from "react";
+import styles from "./background-image.module.css";
+import Logo from "../../../images/logo-1.svg";
+import Mouse from "../../../images/mouse.svg";
+import Carpet_Roll from "../../../videos/trafic.mp4";
 const BackgroundImageUnderContent = () => {
   return (
     <section id={styles.hero}>
-		<div class={styles.background_image_container}>
-		{/* Air  */}
-		<video autoFocus autoPlay loop  src={Carpet_Roll}></video>
+      <div class={styles.background_image_container}>
+        {/* Air  */}
+        <video autoFocus autoPlay loop src={Carpet_Roll}></video>
 
-			<header>
-				<a href="/"><img src={Logo} alt="Logo" /></a>
+        <header>
+          <a href="/">
+            <img src={Logo} alt="Logo" />
+          </a>
 
-				<nav>
-					<ul>
-						<li><a href="#">Work</a></li>
-						<li><a href="#">Company</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">Careers</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-				</nav>
-			</header>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Work</a>
+              </li>
+              <li>
+                <a href="#">Company</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-			<h1>Crafting <span>digital masterpieces</span> with pixel-perfect precision.</h1>
+        <h1>
+          Crafting <span>digital masterpieces</span> with pixel-perfect
+          precision.
+        </h1>
 
-			<img src={Mouse} alt="Mouse" />
-		</div>
-	</section>
-  )
-}
+        <img src={Mouse} alt="Mouse" />
+      </div>
+    </section>
+  );
+};
 
-export default BackgroundImageUnderContent
+export default BackgroundImageUnderContent;
