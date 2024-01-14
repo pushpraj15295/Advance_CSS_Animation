023081eedd4dd483807c83f681fb0b27cb279b36
rishianand39 <rishi.rn818@gmail.com>
2024-01-14 22:20:04 +0530

    path in svg

--- my-css-idea/src/Pages/svgAnimation/svgAnimation.jsx
+++ my-css-idea/src/Pages/svgAnimation/svgAnimation.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const SvgAnimation = () => {
   return (
-    <div>SvgAnimation</div>
-  )
-}
+    <div>
+<svg width="100" height="700" xmlns="http://www.w3.org/2000/svg">
+<path d="M 10 10 A 30 50 45 0 0 900 100" stroke="red" />
 
-export default SvgAnimation
+</svg>
+
+    </div>
+  );
+};
+
+export default SvgAnimation;
